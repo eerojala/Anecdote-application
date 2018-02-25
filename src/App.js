8d3bddf0d0ffb03790a306cbf56ddaceffb443c0
joq tyyppi <joq tyyppi>
2018-02-25 22:47:56 +0200

    6.10 done

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import Notification from './components/Notification'
 import Filter from './components/Filter'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import { connect } from 'react-redux'
+import { anecdoteInitialization } from './reducers/anecdoteReducer'
+import anecdoteService from './services/anecdotes'
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Programming anecdotes</h1>
-        <Notification />
-        <h2>Anecdotes</h2>
-        <Filter />
-        <AnecdoteList />
-        <AnecdoteForm />
-      </div>
-    )
-  }
+    componentDidMount = async () => {
+        const anecdotes = await anecdoteService.getAll()
+        this.props.anecdoteInitialization(anecdotes)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Programming anecdotes</h1>
+                <Notification />
+                <h2>Anecdotes</h2>
+                <Filter />
+                <AnecdoteList />
+                <AnecdoteForm />
+            </div>
+        )
+    }
 }
 
-export default App
+export default connect(null, { anecdoteInitialization })(App)
