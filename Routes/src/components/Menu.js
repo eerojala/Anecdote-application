b3a33589b0d7607427d4d85d45a23d1a3901b39b
joq tyyppi <eero.ojala@helsinki.fi>
2018-03-02 20:05:21 +0200

    6.20 done

--- Routes/src/components/Menu.js
+++ Routes/src/components/Menu.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const style = {
+    borderStyle: 'solid',
+    borderWidth: 5,
+    borderRadius: 5,
+    borderColor: 'cyan',
+    backgroundColor: 'cyan',
+    padding: 5
+}
+
 const Menu = () => (
-    <div>    
+    <div style={style}>    
         <Link to="/anecdotes">Anecdotes</Link>&nbsp;
         <Link to="/createNew">Create new</Link>&nbsp;
         <Link to="/about">About</Link>
